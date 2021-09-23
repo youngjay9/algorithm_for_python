70ef605d9a5751086964bec7b74bb4f554d014bc
JayHung <youngjay9@gmail.com>
2021-09-24 07:10:49 +0800

    binary heap create

--- heap/BinaryHeap.py
+++ heap/BinaryHeap.py
@@ -29,7 +29,7 @@ class BinaryHeap():
             i從 0 開始,找尋有 2 個 child 的 node,
             也就是 heap ditionary 的 index 從 0 到 math.floor(heapSize) 的 node
         ''' 
-        count = math.floor(self.heapSize)
+        count = math.floor(self.heapSize/2)
         while count >=0:
             print(f'i: {count}')
             self.maxHeapify(count)
@@ -67,39 +67,37 @@ if __name__ == "__main__":
     length = 0
     insertElementArray = {}
 
-    node1 = HeapNode('15', 'D')
+    node1 = HeapNode(15, 'D')
     insertElementArray[length] = node1
     length = length +1
 
-    node2 = HeapNode('3', 'A')
+    node2 = HeapNode(3, 'A')
     insertElementArray[length] = node2
     length = length +1
 
-    node3 = HeapNode('17', 'F')
+    node3 = HeapNode(17, 'F')
     insertElementArray[length] = node3
     length = length +1
 
-    node4 = HeapNode('18', 'G')
+    node4 = HeapNode(18, 'G')
     insertElementArray[length] = node4
     length = length +1
 
-    node5 = HeapNode('20', 'H')
+    node5 = HeapNode(20, 'H')
     insertElementArray[length] = node5
     length = length +1
 
-    node6 = HeapNode('2', 'E')
+    node6 = HeapNode(2, 'E')
     insertElementArray[length] = node6
     length = length +1
 
-    node7 = HeapNode('1', 'I')
+    node7 = HeapNode(1, 'I')
     insertElementArray[length] = node7
     length = length +1
 
-    node8 = HeapNode('666', 'B')
+    node8 = HeapNode(666, 'B')
     insertElementArray[length] = node8
     length = length +1
-
-    
 
 
     binaryHeap = BinaryHeap()

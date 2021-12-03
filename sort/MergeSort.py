3c2179c3e00eafc66a16252d7d84b62fe44bd613
Jay hung <35623110+youngjay9@users.noreply.github.com>
2021-12-03 13:29:56 +0800

    Update MergeSort.py
    merge method bug fix.

--- sort/MergeSort.py
+++ sort/MergeSort.py
@@ -1,11 +1,8 @@
 import math
 
 class MergeSort():
-    
-    def __init__(self, arr):
-        self.arr = arr
-    
-    def merge(self, front, end):
+     
+    def merge(self, arr, front, end):
 
         mid = math.floor((front+end)/2)
 
@@ -13,46 +10,48 @@ class MergeSort():
 
         # 將 front ~ mid 放至 leftSub[], 並在最後加入無限大元素
         leftSub = []
-        leftIndx = 0
-        leftCounter = 0
+        leftCounter = front
         while leftCounter <= mid:
-            leftSub.append(self.arr[leftCounter])
+            leftSub.append(arr[leftCounter])
             leftCounter +=1
 
         leftSub.append(positive_infinity)
         
         # 將 mid+1 ~ end 放至 rightSub[], 並在最後加入無限大元素
         rightSub = []
-        rightIndx = 0
         rightCounter = mid+1
         while rightCounter <= end:
-            rightSub.append(self.arr[rightCounter])
+            rightSub.append(arr[rightCounter])
             rightCounter +=1
 
         rightSub.append(positive_infinity)
 
         # loop i from front to end, execute merge
         i = front
+        leftIndx = 0
+        rightIndx = 0
         while i <= end:
             if leftSub[leftIndx] < rightSub[rightIndx]:
-                self.arr[i] = leftSub[leftIndx]
+                arr[i] = leftSub[leftIndx]
                 leftIndx +=1
             else:
-                self.arr[i] = rightSub[rightIndx]
+                arr[i] = rightSub[rightIndx]
                 rightIndx+=1
 
             i+=1
 
-    def mergeSort(self, front, end):
+    def mergeSort(self, arr, front, end):
         
-        if front == end:
-            return self.arr
-        
-        mid = math.floor((front+end)/2)
-        self.mergeSort(front, mid)
-        self.mergeSort(mid+1, end)
-        self.merge(front, end)
-         
+        if front < end:
+            
+            mid = math.floor((front+end)/2)
+            
+            self.mergeSort(arr, front, mid)
+            
+            self.mergeSort(arr, mid+1, end)
+            
+            self.merge(arr, front, end)
+            
 
 
 
@@ -60,9 +59,9 @@ class MergeSort():
 if __name__ == "__main__":
     arr = [3,5,7,6,2,8]
     
-    sort = MergeSort(arr)
+    sort = MergeSort()
     
-    sort.mergeSort(0, len(arr)-1)
+    sort.mergeSort(arr, 0, len(arr)-1)
     
     print(f'{arr}')
     

4457d8c5c185bd38f7eb01c6940f3ec0bb5bad7b
JayHung <youngjay9@gmail.com>
2022-01-16 08:35:15 +0800

    LRUCache add move_to_head() and example.

--- linkedlist/leetCode/LRUCache.py
+++ linkedlist/leetCode/LRUCache.py
@@ -35,10 +35,7 @@ class LRUCache:
                 self.head = newNode
                 self.tail = newNode
             else:
-                newNode.next = self.head
-                self.head.prev = newNode
-                self.head = newNode
-
+                self.move_cur_node_to_haed(newNode)
             # 新增 Node 以後, 超過鏈表的長度需將 tail Node 刪除,以維護 capacity
             if len(self.items) > self.capacity:
                 tail_node = self.tail
@@ -46,19 +43,22 @@ class LRUCache:
                 self.items.pop(tail_node.key)
                 # 變更 tail 指標
                 self.tail = tail_node.prev
+                self.tail.next = None
                 del tail_node
-        # key 存在, 需對該 Node 是 head、tail、others 去處理
+        # key 存在, 根據 node 為 head、tail、others 做以下變更
         else:
             cur_node = self.items[key]
-            if cur_node is self.head:
-                cur_node.val = val
-            elif cur_node is self.tail:
+            cur_node.val = val
+            # 該 node 為 head 只要直接設定 node.val 即可
+            # 該 Node 為 tail
+            if cur_node is self.tail:
                 # 變更 tail 指標
                 self.tail = cur_node.prev
                 self.tail.next = None
                 # 將 cur_node 移至鏈表頭部
                 self.move_cur_node_to_haed(cur_node)
-            else:
+            # 該 node 不為 head 也不為 tail
+            elif cur_node is not self.head:
                 prev_node = cur_node.prev
                 next_node = cur_node.next
                 prev_node.next = next_node
@@ -66,50 +66,67 @@ class LRUCache:
                 # 將 cur_node 移至鏈表頭部
                 self.move_cur_node_to_haed(cur_node)
 
+    '''
+      key 對應的 node 為: 
+      1. head, 則直接回傳 node.val
+      2. tail, 變更 tail 指標, 並將 node 移至鏈表頭部
+      3. others, 將 node 從原本位置刪除,並移至鏈表頭部
+    '''
+
     def get(self, key):
         if key in self.items.keys():
             cur_node = self.items[key]
-
+            # 該 Node 為 tail
             if cur_node is self.tail:
                 # 如果 cur_node tail node, 則變更 tail 指摽指向 cur_node.prev
                 self.tail = cur_node.prev
                 self.tail.next = None
                 # 將 cur_node 移至鏈表頭部
-                cur_node.next = self.head
-                self.head.prev = cur_node
-                self.head = cur_node
-            else:
+                self.move_cur_node_to_haed(cur_node)
+            # 該 node 不為 head 也不為 tail
+            elif cur_node is not self.head:
                 # 將 cur_node 從原本位置刪除
                 prev_node = cur_node.prev
                 next_node = cur_node.next
                 prev_node.next = next_node
                 next_node.prev = prev_node
                 # 將 cur_node 移至鏈表頭部
-                cur_node.next = self.head
-                self.head.prev = cur_node
-                self.head = cur_node
-            return self.items[key].val
+                self.move_cur_node_to_haed(cur_node)
+
+            return cur_node.val
         else:
             return -1
 
     def __repr__(self):
-        vals = []
+        result = []
         cur_node = self.head
         while cur_node is not None:
-            vals.append(str(cur_node.val))
+            result.append(str(cur_node.val))
             cur_node = cur_node.next
-        return '->'.join(vals)
+        return '->'.join(result)
 
 
 if __name__ == "__main__":
     lru_cache = LRUCache(3)
-    lru_cache.put('2', 'Allen')
-    lru_cache.put('3', 'Michele')
-    lru_cache.put('4', 'Mary')
+    lru_cache.put('30', 'curry')
+    lru_cache.put('23', 'Green')
+    lru_cache.put('0', 'Payton')
     print(lru_cache)
 
-    print(f"get(2): {lru_cache.get('2')}")
+    print(f"get(0): {lru_cache.get('0')}")
     print(lru_cache)
 
-    print(f"get(3): {lru_cache.get('3')}")
+    print(f"get(30): {lru_cache.get('30')}")
+    print(lru_cache)
+
+    print(f"get(0): {lru_cache.get('0')}")
+    print(lru_cache)
+
+    print(f"put(11): {lru_cache.put('11','Thompson')}")
+    print(lru_cache)
+
+    print(f"{lru_cache.put('0','Arenas')}")
+    print(lru_cache)
+
+    print(f"{lru_cache.put('30','Wallace')}")
     print(lru_cache)
